ef94288dba14649ff17a6129e49fafc7b987f53c
Brohard <karenbrohard@gmail.com>
2014-12-01 13:42:27 -0800

    added functions

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('index', { title: 'brohard.co' });
 });
+router.get("/about",function(req,res){
+	res.render("about");
+})
 
 module.exports = router;
+
